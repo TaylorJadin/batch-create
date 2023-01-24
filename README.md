894f5b6b00bf94700264f5c333b6e0df890beb42
Taylor Jadin <jadin.taylor@gmail.com>
2023-01-24 14:19:34 +0000

    disabled email

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Create sites and/or users or add users to existing sites by uloading a CSV file.
 ## Differences from the WPMU plugin
 - This works on modern WordPress and PHP as of writing this (WP 6.1.1 and PHP 8)
 - It uses email address, not username as the unique identifier for looking up Users. If an email is already in use, it will find the username that corresponds with that email and proceed.
+- I've disabled welcome emails to users who are added to sites.
 - I've removed XLS support and streamlined the instructions page.
 - The CSV template is tweaked a bit.
 
--- admin/pages/network-main-page.php
+++ admin/pages/network-main-page.php
@@ -239,7 +239,7 @@ class Batch_Create_Network_Main_Menu extends Origin_Admin_Page {
 		?>
 			<h3><?php _e( 'Instructions', INCSUB_BATCH_CREATE_LANG_DOMAIN ); ?></h3>
 			<p><?php _e( "Batch create is designed for quickly creating sites and/or usernames or adding users to an existing site in batches by uploading a CSV file.", INCSUB_BATCH_CREATE_LANG_DOMAIN ); ?></p>
-			<p><?php _e( "Note that Batch Create will trigger welcome emails to send as if you were managing sites and user manually." ); ?></p>
+			<p><?php _e( "Note that Batch Create will not send welcome emails to users who are added to sites, but Super Admins may still receive site creation and user registration notification emails." ); ?></p>
 
 			<ol>
 				<li><?php printf( __( 'Download <a href="%s">this .csv</a> file and use it as a template to create your batch file.'), $test_csv_url ); ?></li>
